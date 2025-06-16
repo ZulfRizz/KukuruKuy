f5388d8d8e8f86128f57adaeda1b9ed40125e24c
ZulfRizz <zulfrianwar@gmail.com>
2025-06-17 00:56:48 +0700

    minor fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -51,5 +52,14 @@ class User extends Authenticatable implements MustVerifyEmail {
      */
     public function franchise(): BelongsTo {
         return $this->belongsTo(Franchise::class);
+    }
+
+    /**
+     * Menentukan apakah pengguna dapat mengakses Panel Filament.
+     * Ini adalah fungsi validasi yang dicari oleh Filament.
+     */
+    public function canAccessPanel(Panel $panel): bool {
+        // Hanya izinkan akses jika peran pengguna adalah 'admin' 
+        return $this->role === 'admin';
     }
 }
